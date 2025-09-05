6b8783c9dee52359be6088a306ff85ca887a24e6
Nikolay Todorov <everflowing555@gmail.com>
2025-09-05 18:10:35 +0300

    Addec UserTmRepoCancellationTests

---
+++ IPNoticeHub.Tests/UnitTests/RepositoryTests/Trademarks/UserTrademarkRepositoryTests/UserTmRepoCancellationTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using IPNoticeHub.Common.EnumConstants;
+using IPNoticeHub.Data.Repositories.Trademarks.Implementations;
+using IPNoticeHub.Tests.TestUtilities;
+using NUnit.Framework;
+
+namespace IPNoticeHub.Tests.UnitTests.RepositoryTests.Trademarks.UserTrademarkRepositoryTests
+{
+    /// <summary>
+    /// Verifies that repository methods respect a pre-cancelled CancellationToken 
+    /// and propagate OperationCanceledException or TaskCanceledException.
+    /// Ensures database operations halt promptly during upstream cancellations.
+    /// </summary>
+    [TestFixture]
+    public class UserTmRepoCancellationTests
+    {
+        [Test]
+        public async Task AddOrUndeleteAsync_ThrowsException_WhenCancellationRequested()
+        {
+            using var testDbContext = InMemoryDbContextFactory.CreateTestDbContext();
+
+            var user = InMemoryDbContextFactory.CreateApplicationUser("user1");
+            testDbContext.Users.Add(user);
+
+            var (trademarkEntity, _) = InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "Moon Apes",
+                owner: "Default Owner",
+                regNumber: "1234567",
+                TrademarkStatusCategory.Registered,
+                DataProvider.USPTO);
+
+            testDbContext.TrademarkRegistrations.Add(trademarkEntity);
+
+            await testDbContext.SaveChangesAsync();
+
+            var userTmRepository = new UserTrademarkRepository(testDbContext);
+
+            await userTmRepository.AddOrUndeleteAsync(user.Id, trademarkEntity.Id);
+
+            using var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.Cancel();
+
+            await FluentActions.Awaiting(() => userTmRepository.AddOrUndeleteAsync(user.Id, trademarkEntity.Id, cancellationTokenSource.Token))
+                .Should().ThrowAsync<OperationCanceledException>();
+        }
+
+        [Test]
+        public async Task SoftRemoveAsync_Throws_WhenCancellationRequested()
+        {
+            using var testDbContext = InMemoryDbContextFactory.CreateTestDbContext();
+
+            var user = InMemoryDbContextFactory.CreateApplicationUser("user1");
+            testDbContext.Users.Add(user);
+
+            var (trademarkEntity, _) = InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "Moon Apes",
+                owner: "Default Owner",
+                regNumber: "1234567",
+                TrademarkStatusCategory.Registered,
+                DataProvider.USPTO);
+
+            testDbContext.TrademarkRegistrations.Add(trademarkEntity);
+
+            await testDbContext.SaveChangesAsync();
+
+            var userTmRepository = new UserTrademarkRepository(testDbContext);
+
+            await userTmRepository.AddOrUndeleteAsync(user.Id, trademarkEntity.Id);
+
+            using var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.Cancel();
+
+            await FluentActions.Awaiting(() => userTmRepository.SoftRemoveAsync(user.Id, trademarkEntity.Id, cancellationTokenSource.Token))
+                .Should().ThrowAsync<OperationCanceledException>();
+        }
+    }
+}
