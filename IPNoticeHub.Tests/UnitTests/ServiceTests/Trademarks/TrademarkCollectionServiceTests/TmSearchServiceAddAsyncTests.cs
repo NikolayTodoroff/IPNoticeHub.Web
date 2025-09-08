7af0669eb8e9d9bb624c37b7c612dfdc9a89ccea
Nikolay Todorov <everflowing555@gmail.com>
2025-09-08 14:29:12 +0300

    Added TmSearchServiceAddAsyncTests

---
+++ IPNoticeHub.Tests/UnitTests/ServiceTests/Trademarks/TrademarkCollectionServiceTests/TmSearchServiceAddAsyncTests.cs
@@ -0,0 +1,139 @@
+﻿using FluentAssertions;
+using IPNoticeHub.Common.EnumConstants;
+using IPNoticeHub.Data;
+using IPNoticeHub.Data.Entities.ApplicationUser;
+using IPNoticeHub.Data.Repositories.Trademarks.Abstractions;
+using IPNoticeHub.Data.Repositories.Trademarks.Implementations;
+using IPNoticeHub.Services.Trademarks.Implementations;
+using IPNoticeHub.Tests.TestUtilities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPNoticeHub.Tests.UnitTests.ServiceTests.Trademarks.TrademarkCollectionServiceTests
+{
+    [TestFixture]
+    /// <summary>
+    /// Section: TrademarkCollectionService - AddAsync behaviour.
+    /// - Verifies that when a trademark is not already in the user's collection, it is added successfully,
+    /// and subsequent checks confirm its presence in the collection.
+    /// - Verifies that if a soft-deleted link exists, AddAsync reactivates the same link (IsDeleted=false).
+    /// </summary>
+    [TestFixture]
+    public class TmSearchServiceAddAsyncTests
+    {
+        [Test]
+        public async Task AddAsync_WhenNotInCollection_AddsLink_ThenIsInCollectionReturnsTrue()
+        {
+            using var testDbContext = InMemoryDbContextFactory.CreateTestDbContext();
+
+            var user = new ApplicationUser
+            {
+                Id = "user-1",
+                UserName = "newUser",
+                Email = "user1@test.local"
+            };
+
+            var (trademarkEntity, _) = InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "AAA",
+                owner: "Owner A",
+                regNumber: "1234567",
+                status: TrademarkStatusCategory.Registered,
+                source: DataProvider.USPTO,
+                classNumbers: new[] { 9, 35 });
+
+            testDbContext.Users.Add(user);
+
+            testDbContext.TrademarkRegistrations.Add(trademarkEntity);
+            await testDbContext.SaveChangesAsync();
+
+            ITrademarkRepository tmRepository = new TrademarkRepository(testDbContext);
+            IUserTrademarkRepository userTmRepository = new UserTrademarkRepository(testDbContext);
+
+            var service = new TrademarkCollectionService(tmRepository, userTmRepository);
+
+            await service.AddAsync(
+                userId: user.Id,
+                trademarkId: trademarkEntity.Id,
+                cancellationToken: default);
+
+            bool linkedInCollection = await service.IsInCollectionAsync(
+                user.Id, trademarkEntity.Id, includeSoftDeleted: false, cancellationToken: default);
+
+            linkedInCollection.Should().BeTrue();
+
+            var existingLink = testDbContext.UserTrademarks.Where(x => x.ApplicationUserId == user.Id && x.TrademarkRegistrationId == trademarkEntity.Id).ToList();
+            existingLink.Should().HaveCount(1);
+            existingLink[0].IsDeleted.Should().BeFalse();
+        }
+
+        [Test]
+        public async Task AddAsync_WhenPreviouslySoftDeleted_UndeletesExistingLink()
+        {
+            using var testDbContext = InMemoryDbContextFactory.CreateTestDbContext();
+
+            var user = new ApplicationUser
+            {
+                Id = "user-1",
+                UserName = "newUser",
+                Email = "user1@test.local"
+            };
+
+            var (trademarkEntity, _) = InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "AAA",
+                owner: "Owner A",
+                regNumber: "1234567",
+                status: TrademarkStatusCategory.Registered,
+                source: DataProvider.USPTO,
+                classNumbers: new[] { 9, 35 });
+
+            testDbContext.Users.Add(user);
+
+            testDbContext.TrademarkRegistrations.Add(trademarkEntity);
+            await testDbContext.SaveChangesAsync();
+
+            ITrademarkRepository tmRepository = new TrademarkRepository(testDbContext);
+            IUserTrademarkRepository userTmRepository = new UserTrademarkRepository(testDbContext);
+
+            var service = new TrademarkCollectionService(tmRepository, userTmRepository);
+
+            // Create a link, then soft-delete it, assert that the link exists but is soft-deleted
+            await service.AddAsync(
+                userId: user.Id,
+                trademarkId: trademarkEntity.Id,
+                cancellationToken: default);
+
+            await service.RemoveAsync(
+                userId: user.Id,
+                trademarkId: trademarkEntity.Id,
+                cancellationToken: default);
+
+            var softDeletedLinks = testDbContext.UserTrademarks.Where(
+                x => x.ApplicationUserId == user.Id && x.TrademarkRegistrationId == trademarkEntity.Id).ToList();
+
+            softDeletedLinks.Should().HaveCount(1);
+            softDeletedLinks[ index: 0 ].IsDeleted.Should().BeTrue();
+
+            // AddAsync again should "undelete" the same link, assert that the link is active again
+            await service.AddAsync(
+               userId: user.Id,
+               trademarkId: trademarkEntity.Id,
+               cancellationToken: default);
+
+            var unDeletedLinks = testDbContext.UserTrademarks.Where(
+                x => x.ApplicationUserId == user.Id && x.TrademarkRegistrationId == trademarkEntity.Id).ToList();
+
+            unDeletedLinks.Should().HaveCount(1);
+            unDeletedLinks[index: 0].IsDeleted.Should().BeFalse();
+
+
+            bool linkedInCollection = await service.IsInCollectionAsync(
+                user.Id, trademarkEntity.Id, includeSoftDeleted: false, cancellationToken: default);
+
+            linkedInCollection.Should().BeTrue();
+        }
+    }
+}
