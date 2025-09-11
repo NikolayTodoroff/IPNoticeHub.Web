61e23b8e95f9d122b4a76dc593ea6a57fe435b8a
Nikolay Todorov <everflowing555@gmail.com>
2025-09-11 21:52:01 +0300

    Added CopyrightEditDTO

---
+++ Services/Copyrights/DTOs/CopyrightEditDTO.cs
@@ -0,0 +1,31 @@
+﻿using IPNoticeHub.Common.EnumConstants;
+using System.ComponentModel.DataAnnotations;
+using static IPNoticeHub.Common.ValidationConstants.CopyrightRegistrationConstants;
+
+namespace IPNoticeHub.Services.Copyrights.DTOs
+{
+    public class CopyrightEditDTO
+    {
+        [Required, MaxLength(RegistrationNumberMaxLength)]
+        public string RegistrationNumber { get; init; } = string.Empty;
+
+        [Required]
+        public CopyrightWorkType WorkType { get; init; }
+
+        [MaxLength(WorkTypeMaxLength)]
+        public string? OtherWorkType { get; init; }
+
+        [Required, MaxLength(TitleMaxLength)]
+        public string Title { get; init; } = string.Empty;
+
+        public int? YearOfCreation { get; init; }
+
+        public DateTime? DateOfPublication { get; init; }
+
+        [Required, MaxLength(OwnerNameMaxLength)]
+        public string Owner { get; init; } = string.Empty;
+
+        [MaxLength(NationOfFirstPublicationMaxLength)]
+        public string? NationOfFirstPublication { get; init; }
+    }
+}
