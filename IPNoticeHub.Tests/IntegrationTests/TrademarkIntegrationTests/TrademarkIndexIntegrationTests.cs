bd7eecf014a7fd580bd56ec0348bd412929c262f
Nikolay Todorov <everflowing555@gmail.com>
2025-09-17 21:30:25 +0300

    added more tests to TrademarkMyCollectionIntegrationTests

---
+++ IPNoticeHub.Tests/IntegrationTests/TrademarkIntegrationTests/TrademarkIndexIntegrationTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using IPNoticeHub.Common.EnumConstants;
+using IPNoticeHub.Data;
+using IPNoticeHub.Data.Entities.ApplicationUser;
+using IPNoticeHub.Data.Entities.TrademarkRegistration;
+using IPNoticeHub.Tests.IntegrationTests.TestUtilities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System.Net;
+
+namespace IPNoticeHub.Tests.IntegrationTests.TrademarkIntegrationTests
+{
+    public class TrademarkIndexIntegrationTests
+    {
+    }
+}
--- IPNoticeHub.Tests/IntegrationTests/TrademarkIntegrationTests/TrademarkMyCollectionIntegrationTests.cs
+++ IPNoticeHub.Tests/IntegrationTests/TrademarkIntegrationTests/TrademarkMyCollectionIntegrationTests.cs
@@ -4,9 +4,15 @@ using IPNoticeHub.Data;
 using IPNoticeHub.Data.Entities.ApplicationUser;
 using IPNoticeHub.Data.Entities.TrademarkRegistration;
 using IPNoticeHub.Tests.IntegrationTests.TestUtilities;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using NUnit.Framework;
 using System.Net;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.Extensions.Logging;
 
 namespace IPNoticeHub.Tests.IntegrationTests.TrademarkIntegrationTests
 {
@@ -32,5 +38,399 @@ namespace IPNoticeHub.Tests.IntegrationTests.TrademarkIntegrationTests
 
             response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
         }
+
+        [Test]
+        public async Task Get_MyCollection_AuthenticatedWithoutNameIdentifier_Returns403()
+        {
+            var layeredFactory = appFactory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services
+                        .AddAuthentication(authOptions =>
+                        {
+                            authOptions.DefaultAuthenticateScheme = "NoId";
+                            authOptions.DefaultChallengeScheme = "NoId";
+                        })
+                        .AddScheme<AuthenticationSchemeOptions, NoIdAuthHandler>("NoId", _ => { });
+                });
+            });
+
+            await using (layeredFactory)
+            {
+                var client = layeredFactory.CreateClient(new() { AllowAutoRedirect = false });
+                var response = await client.GetAsync("/Trademarks/MyCollection");
+
+                response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+            }
+        }
+
+        private sealed class NoIdAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+        {
+            public NoIdAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
+                                   ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder) { }
+
+            protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, "AuthNoId"),
+                    new Claim(ClaimTypes.Email, "authnoid@test.local")
+                    // intentionally no ClaimTypes.NameIdentifier
+                }, "NoId");
+
+                var claimsPrincipal = new ClaimsPrincipal(identity);
+                var authTicket = new AuthenticationTicket(claimsPrincipal, "NoId");
+
+                return Task.FromResult(AuthenticateResult.Success(authTicket));
+            }
+        }
+
+        [Test]
+        public async Task Get_MyCollection_WithPagingAndSorting_Returns200()
+        {
+            var userId = "u1";
+            var client = appFactory.CreateClientAs(userId);
+
+            using (var serviceScope = appFactory.Services.CreateScope())
+            {
+                var testDbContext = serviceScope.ServiceProvider.GetRequiredService<IPNoticeHubDbContext>();
+
+                await TestDbSeeder.SeedUserAsync(testDbContext, userId);
+
+                var entity1 = new TrademarkEntity
+                {
+                    Wordmark = "First Entity",
+                    SourceId = "US-PAGE-A",
+                    RegistrationNumber = "RN-A",
+                    GoodsAndServices = "Software",
+                    Owner = "Acme",
+                    StatusCategory = TrademarkStatusCategory.Pending,
+                    StatusDetail = "Pending",
+                    Source = DataProvider.USPTO
+                };
+
+                var entity2 = new TrademarkEntity
+                {
+                    Wordmark = "Second Entity",
+                    SourceId = "US-PAGE-B",
+                    RegistrationNumber = "RN-B",
+                    GoodsAndServices = "Software",
+                    Owner = "Acme",
+                    StatusCategory = TrademarkStatusCategory.Pending,
+                    StatusDetail = "Pending",
+                    Source = DataProvider.USPTO
+                };
+
+                var entity3 = new TrademarkEntity
+                {
+                    Wordmark = "Third Entity",
+                    SourceId = "US-PAGE-C",
+                    RegistrationNumber = "RN-C",
+                    GoodsAndServices = "Software",
+                    Owner = "Acme",
+                    StatusCategory = TrademarkStatusCategory.Pending,
+                    StatusDetail = "Pending",
+                    Source = DataProvider.USPTO
+                };
+
+                testDbContext.TrademarkRegistrations.AddRange(entity1, entity2, entity3);
+                await testDbContext.SaveChangesAsync();
+
+                testDbContext.UserTrademarks.AddRange(
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = entity1.Id, IsDeleted = false },
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = entity2.Id, IsDeleted = false },
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = entity3.Id, IsDeleted = false }
+                );
+
+                await testDbContext.SaveChangesAsync();
+            }
+
+            var response = await client.GetAsync("/Trademarks/MyCollection?sortBy=WordmarkAsc&currentPage=2&resultsPerPage=1");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task Get_MyCollection_WithOutOfRangePaging_Returns200()
+        {
+            var userId = "u1";
+
+            var client = appFactory.CreateClientAs(userId);
+
+            using (var serviceScope = appFactory.Services.CreateScope())
+            {
+                var testDbContext = serviceScope.ServiceProvider.GetRequiredService<IPNoticeHubDbContext>();
+                await TestDbSeeder.SeedUserAsync(testDbContext, userId);
+
+                var entity1 = new TrademarkEntity
+                {
+                    Wordmark = "First WM",
+                    SourceId = "US-OUTR-1",
+                    RegistrationNumber = "RN-1",
+                    GoodsAndServices = "Software",
+                    Owner = "Owner1",
+                    StatusCategory = TrademarkStatusCategory.Pending,
+                    StatusDetail = "Pending",
+                    Source = DataProvider.USPTO
+                };
+                var entity2 = new TrademarkEntity
+                {
+                    Wordmark = "Second WM",
+                    SourceId = "US-OUTR-2",
+                    RegistrationNumber = "RN-2",
+                    GoodsAndServices = "Software",
+                    Owner = "Owner2",
+                    StatusCategory = TrademarkStatusCategory.Pending,
+                    StatusDetail = "Pending",
+                    Source = DataProvider.USPTO
+                };
+
+                testDbContext.TrademarkRegistrations.AddRange(entity1, entity2);
+                await testDbContext.SaveChangesAsync();
+
+                testDbContext.UserTrademarks.AddRange(
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = entity1.Id, IsDeleted = false },
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = entity2.Id, IsDeleted = false }
+                );
+
+                await testDbContext.SaveChangesAsync();
+            }
+
+            var response = await client.GetAsync("/Trademarks/MyCollection?sortBy=WordmarkAsc&currentPage=9999&resultsPerPage=9999");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task Get_MyCollection_InvalidSortParameter_Returns200()
+        {
+            var userId = "u1";
+
+            var client = appFactory.CreateClientAs(userId);
+
+            using (var serviceScope = appFactory.Services.CreateScope())
+            {
+                var testDbContext = serviceScope.ServiceProvider.GetRequiredService<IPNoticeHubDbContext>();
+                await TestDbSeeder.SeedUserAsync(testDbContext, userId);
+
+                var entity1 = new TrademarkEntity
+                {
+                    Wordmark = "WM1",
+                    SourceId = "US-SORT-1",
+                    RegistrationNumber = "RN-1",
+                    GoodsAndServices = "Software",
+                    Owner = "Acme",
+                    StatusCategory = TrademarkStatusCategory.Pending,
+                    StatusDetail = "Pending",
+                    Source = DataProvider.USPTO
+                };
+
+                var entity2 = new TrademarkEntity
+                {
+                    Wordmark = "WM2",
+                    SourceId = "US-SORT-2",
+                    RegistrationNumber = "RN-2",
+                    GoodsAndServices = "Software",
+                    Owner = "Acme",
+                    StatusCategory = TrademarkStatusCategory.Pending,
+                    StatusDetail = "Pending",
+                    Source = DataProvider.USPTO
+                };
+
+                testDbContext.TrademarkRegistrations.AddRange(entity1, entity2);
+                await testDbContext.SaveChangesAsync();
+
+                testDbContext.UserTrademarks.AddRange(
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = entity1.Id, IsDeleted = false },
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = entity2.Id, IsDeleted = false }
+                );
+                await testDbContext.SaveChangesAsync();
+            }
+
+            var response = await client.GetAsync("/Trademarks/MyCollection?sortBy=TotallyNotAValue&currentPage=1&resultsPerPage=10");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task Get_MyCollection_IgnoresSoftDeletedLinks_Returns200()
+        {
+            var userId = "u1";
+            var client = appFactory.CreateClientAs(userId);
+
+            using (var serviceScope = appFactory.Services.CreateScope())
+            {
+                var testDbContext = serviceScope.ServiceProvider.GetRequiredService<IPNoticeHubDbContext>();
+                await TestDbSeeder.SeedUserAsync(testDbContext, userId);
+
+                var activeEntity = new TrademarkEntity
+                {
+                    Wordmark = "ACTIVE-MARK",
+                    SourceId = "US-SDEL-A",
+                    RegistrationNumber = "RN-A",
+                    GoodsAndServices = "Software",
+                    Owner = "Acme",
+                    StatusCategory = TrademarkStatusCategory.Registered,
+                    StatusDetail = "Registered",
+                    Source = DataProvider.USPTO
+                };
+
+                var deletedEntity = new TrademarkEntity
+                {
+                    Wordmark = "DELETED-MARK",
+                    SourceId = "US-SDEL-D",
+                    RegistrationNumber = "RN-D",
+                    GoodsAndServices = "Software",
+                    Owner = "Acme",
+                    StatusCategory = TrademarkStatusCategory.Registered,
+                    StatusDetail = "Registered",
+                    Source = DataProvider.USPTO
+                };
+
+                testDbContext.TrademarkRegistrations.AddRange(activeEntity, deletedEntity);
+                await testDbContext.SaveChangesAsync();
+
+                testDbContext.UserTrademarks.AddRange(
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = activeEntity.Id, IsDeleted = false },
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = deletedEntity.Id, IsDeleted = true }
+                );
+                await testDbContext.SaveChangesAsync();
+            }
+
+            var response = await client.GetAsync("/Trademarks/MyCollection?sortBy=WordmarkAsc&currentPage=1&resultsPerPage=10");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task Get_MyCollection_WhenCollectionIsEmpty_Returns200()
+        {
+            var userId = "u1";
+            var client = appFactory.CreateClientAs(userId);
+
+            using (var serviceScope = appFactory.Services.CreateScope())
+            {
+                var testDbContext = serviceScope.ServiceProvider.GetRequiredService<IPNoticeHubDbContext>();
+                await TestDbSeeder.SeedUserAsync(testDbContext, userId);
+            }
+
+            var response = await client.GetAsync("/Trademarks/MyCollection");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [TestCase(0)]
+        [TestCase(-5)]
+        public async Task Get_MyCollection_WhenResultsPerPage_NonPositive_Returns200(int resultsPerPage)
+        {
+            var userId = "u1";
+            var client = appFactory.CreateClientAs(userId);
+
+            using (var serviceScope = appFactory.Services.CreateScope())
+            {
+                var testDbContext = serviceScope.ServiceProvider.GetRequiredService<IPNoticeHubDbContext>();
+                await TestDbSeeder.SeedUserAsync(testDbContext, userId);
+
+                var entity1 = new TrademarkEntity
+                {
+                    Wordmark = "A1",
+                    SourceId = "US-1111",
+                    RegistrationNumber = "RN-A",
+                    GoodsAndServices = "Software",
+                    Owner = "Acme", 
+                    StatusCategory = TrademarkStatusCategory.Registered,
+                    StatusDetail = "Registered",
+                    Source = DataProvider.USPTO };
+
+                var entity2 = new TrademarkEntity
+                {
+                    Wordmark = "B2",
+                    SourceId = "US-2222",
+                    RegistrationNumber = "RN-B",
+                    GoodsAndServices = "Software",
+                    Owner = "Acme", 
+                    StatusCategory = TrademarkStatusCategory.Registered,
+                    StatusDetail = "Registered",
+                    Source = DataProvider.USPTO };
+
+                testDbContext.TrademarkRegistrations.AddRange(entity1, entity2);
+                await testDbContext.SaveChangesAsync();
+
+                testDbContext.UserTrademarks.AddRange(
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = entity1.Id, IsDeleted = false },
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = entity2.Id, IsDeleted = false }
+                );
+                await testDbContext.SaveChangesAsync();
+            }
+
+            var response = await client.GetAsync($"/Trademarks/MyCollection?sortBy=WordmarkAsc&currentPage=1&resultsPerPage={resultsPerPage}");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [TestCase(5000)]
+        [TestCase(10000)]
+        public async Task Get_MyCollection_ResultsPerPage_VeryLarge_Returns200(int resultsPerPage)
+        {
+            var userId = "u1";
+            var client = appFactory.CreateClientAs(userId);
+
+            using (var serviceScope = appFactory.Services.CreateScope())
+            {
+                var testDbContext = serviceScope.ServiceProvider.GetRequiredService<IPNoticeHubDbContext>();
+                await TestDbSeeder.SeedUserAsync(testDbContext, userId);
+
+                var entity1 = new TrademarkEntity
+                { 
+                    Wordmark = "A1",
+                    SourceId = "US-1111",
+                    RegistrationNumber = "RN-A",
+                    GoodsAndServices = "Software",
+                    Owner = "Acme", 
+                    StatusCategory = TrademarkStatusCategory.Registered,
+                    StatusDetail = "Registered",
+                    Source = DataProvider.USPTO };
+
+                var entity2 = new TrademarkEntity
+                { 
+                    Wordmark = "B2",
+                    SourceId = "US-2222",
+                    RegistrationNumber = "RN-B",
+                    GoodsAndServices = "Software",
+                    Owner = "Acme",
+                    StatusCategory = TrademarkStatusCategory.Registered,
+                    StatusDetail = "Registered",
+                    Source = DataProvider.USPTO };
+
+                var entity3 = new TrademarkEntity
+                { 
+                    Wordmark = "C3",
+                    SourceId = "US-3333",
+                    RegistrationNumber = "RN-C",
+                    GoodsAndServices = "Software",
+                    Owner = "Acme",
+                    StatusCategory = TrademarkStatusCategory.Registered,
+                    StatusDetail = "Registered",
+                    Source = DataProvider.USPTO };
+
+                testDbContext.TrademarkRegistrations.AddRange(entity1, entity2, entity3);
+                await testDbContext.SaveChangesAsync();
+
+                testDbContext.UserTrademarks.AddRange(
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = entity1.Id, IsDeleted = false },
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = entity2.Id, IsDeleted = false },
+                    new UserTrademark { ApplicationUserId = userId, TrademarkRegistrationId = entity3.Id, IsDeleted = false }
+                );
+
+                await testDbContext.SaveChangesAsync();
+            }
+
+            var response = await client.GetAsync($"/Trademarks/MyCollection?sortBy=WordmarkAsc&currentPage=1&resultsPerPage={resultsPerPage}");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
     }
 }
+
+
