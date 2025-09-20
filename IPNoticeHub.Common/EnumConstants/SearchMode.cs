03c434c66c47f10afe6e4c213a3589b9bf29600e
Nikolay Todorov <everflowing555@gmail.com>
2025-09-20 13:04:12 +0300

    Added enum class SearchMode; Changed Search Mode Items from hard-coded strings to the corresponding enum

---
+++ IPNoticeHub.Common/EnumConstants/SearchMode.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IPNoticeHub.Common.EnumConstants
+{
+    /// <summary>
+    /// Represents the search modes used for filtering trademarks.
+    /// </summary>
+    public enum SearchMode
+    {
+        [Display(Name = "Identical")]
+        Identical = 0,
+
+        [Display(Name = "Contains")]
+        Contains = 1
+    }
+}
