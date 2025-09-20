0ea7dbcba1644af45ed0410506be8793c336da61
Nikolay Todorov <everflowing555@gmail.com>
2025-09-20 15:05:03 +0300

    Added SearchResultsViewModel and _TrademarksSearchResults

--- IPNoticeHub.Web/Controllers/HomeController.cs
+++ IPNoticeHub.Web/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-using System.Diagnostics;
+using IPNoticeHub.Common.EnumConstants;
 using IPNoticeHub.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+using IPNoticeHub.Web.Models.Trademarks;
 
 namespace IPNoticeHub.Web.Controllers
 {
@@ -21,6 +24,24 @@ namespace IPNoticeHub.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Results(string? tmQuery, TrademarkClass? tmClass, TrademarkStatusCategory? tmStatus,
+        TrademarkSearchBy? tmSearchItem, DataProvider? tmOffice, SearchMode? tmMode,int currentPage = 1, int pageSize = 25)
+        {
+            var resultViewModel = new TrademarkSearchResultsViewModel
+            {
+                Query = tmQuery,
+                Class = tmClass,
+                Status = tmStatus,
+                SearchBy = tmSearchItem,
+                Office = tmOffice,
+                Mode = tmMode,
+                Results = Enumerable.Empty<TrademarkResultRowViewModel>(),
+                Total = 0
+            };
+            return View(resultViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ IPNoticeHub.Web/Models/Trademarks/TrademarkSearchResultsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using IPNoticeHub.Common.EnumConstants;
+
+namespace IPNoticeHub.Web.Models.Trademarks
+{
+    public class TrademarkSearchResultsViewModel
+    {
+        public string? Query { get; set; }
+        public TrademarkClass? Class { get; set; }
+        public TrademarkStatusCategory? Status { get; set; }
+        public TrademarkSearchBy? SearchBy { get; set; }
+        public DataProvider? Office { get; set; }
+        public SearchMode? Mode { get; set; }
+        public IEnumerable<TrademarkResultRowViewModel> Results { get; set; } = 
+            Enumerable.Empty<TrademarkResultRowViewModel>();
+        public int Total { get; set; }
+    }
+
+    public class TrademarkResultRowViewModel
+    {
+        public string RegistrationNumber { get; set; } = "";
+        public string Mark { get; set; } = "";
+        public string Owner { get; set; } = "";
+        public string Status { get; set; } = "";
+    }
+}
