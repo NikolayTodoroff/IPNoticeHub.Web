c466cd27af48b1b04db2a2399546b8b05edef8da
Nikolay Todorov <everflowing555@gmail.com>
2025-09-22 17:30:32 +0300

    Added TmSearchQueryPagingTests

--- IPNoticeHub.Tests/UnitTests/ServiceTests/TrademarkServiceTests/TrademarkSearchQueryServiceTests/TmSearchQueryPagingTests.cs
+++ IPNoticeHub.Tests/UnitTests/ServiceTests/TrademarkServiceTests/TrademarkSearchQueryServiceTests/TmSearchQueryPagingTests.cs
@@ -12,5 +12,153 @@ namespace IPNoticeHub.Tests.UnitTests.ServiceTests.TrademarkServiceTests.Tradema
 {
     public class TmSearchQueryPagingTests
     {
+        [Test]
+        public async Task SearchAsync_Paging_ReturnsSecondItemOnPage2_AndKeepsTotal()
+        {
+            using IPNoticeHubDbContext testDbContext = InMemoryDbContextFactory.CreateTestDbContext();
+
+            var (anubisTm, _) = InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "Anubis",
+                owner: "Underworld Inc.",
+                regNumber: "1111111",
+                status: TrademarkStatusCategory.Registered,
+                source: DataProvider.USPTO,
+                classNumbers: new[] { 25 });
+
+            var (horusTm, _) = InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "Horus",
+                owner: "Falcon LLC",
+                regNumber: "2222222",
+                status: TrademarkStatusCategory.Registered,
+                source: DataProvider.USPTO,
+                classNumbers: new[] { 25 });
+
+            var (osirisTm, _) = InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "Osiris",
+                owner: "Afterlife Inc.",
+                regNumber: "3333333",
+                status: TrademarkStatusCategory.Registered,
+                source: DataProvider.USPTO,
+                classNumbers: new[] { 25 });
+
+            testDbContext.TrademarkRegistrations.AddRange(anubisTm, horusTm, osirisTm);
+            await testDbContext.SaveChangesAsync();
+
+            ITrademarkReadRepository readRepo = new TestReadRepository(testDbContext);
+            var queryService = new TrademarkSearchQueryService(readRepo);
+
+            var query = new TrademarkSearchQueryDTO
+            {
+                Query = "",
+                SearchBy = TrademarkSearchBy.Wordmark,
+                Mode = SearchMode.Contains,
+                Page = 2,
+                PageSize = 1
+            };
+
+            var (queryResult, total) = await queryService.SearchAsync(query, CancellationToken.None);
+            total.Should().Be(3);
+            queryResult.Single().RegistrationNumber.Should().Be("2222222");
+        }
+
+        [Test]
+        public async Task SearchAsync_Paging_OutOfRange_ReturnsEmpty_AndKeepsTotal()
+        {
+            using IPNoticeHubDbContext testDbContext = InMemoryDbContextFactory.CreateTestDbContext();
+
+            var (anubisTm, _) = InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "Anubis",
+                owner: "Underworld Inc.",
+                regNumber: "1111111",
+                status: TrademarkStatusCategory.Registered,
+                source: DataProvider.USPTO,
+                classNumbers: new[] { 25 });
+
+            var (horusTm, _) = InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "Horus",
+                owner: "Falcon LLC",
+                regNumber: "2222222",
+                status: TrademarkStatusCategory.Registered,
+                source: DataProvider.USPTO,
+                classNumbers: new[] { 25 });
+
+            var (osirisTm, _) = InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "Osiris",
+                owner: "Afterlife Inc.",
+                regNumber: "3333333",
+                status: TrademarkStatusCategory.Registered,
+                source: DataProvider.USPTO,
+                classNumbers: new[] { 25 });
+
+            testDbContext.TrademarkRegistrations.AddRange(anubisTm, horusTm, osirisTm);
+            await testDbContext.SaveChangesAsync();
+
+            ITrademarkReadRepository readRepo = new TestReadRepository(testDbContext);
+            var queryService = new TrademarkSearchQueryService(readRepo);
+
+            var query = new TrademarkSearchQueryDTO
+            {
+                Query = "",
+                SearchBy = TrademarkSearchBy.Wordmark,
+                Mode = SearchMode.Contains,
+                Page = 3, //Out of range
+                PageSize = 2
+            };
+
+            var (queryResult, total) = await queryService.SearchAsync(query, CancellationToken.None);
+            total.Should().Be(3);
+            queryResult.Should().BeEmpty();
+        }
+
+        [Test]
+        public async Task SearchAsync_WithEmptyQueryAndNoFilters_ReturnsAll_OrderedByRegistrationNumber()
+        {
+            using IPNoticeHubDbContext testDbContext = InMemoryDbContextFactory.CreateTestDbContext();
+
+            var (anubisTm, _) = InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "Anubis",
+                owner: "Underworld Inc.",
+                regNumber: "1111111",
+                status: TrademarkStatusCategory.Registered,
+                source: DataProvider.USPTO,
+                classNumbers: new[] { 25 });
+
+            var (horusTm, _) = InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "Horus",
+                owner: "Falcon LLC",
+                regNumber: "2222222",
+                status: TrademarkStatusCategory.Registered,
+                source: DataProvider.USPTO,
+                classNumbers: new[] { 25 });
+
+            var (osirisTm, _) = InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "Osiris",
+                owner: "Afterlife Inc.",
+                regNumber: "3333333",
+                status: TrademarkStatusCategory.Registered,
+                source: DataProvider.USPTO,
+                classNumbers: new[] { 25 });
+
+            testDbContext.TrademarkRegistrations.AddRange(anubisTm, horusTm, osirisTm);
+            await testDbContext.SaveChangesAsync();
+
+            ITrademarkReadRepository readRepo = new TestReadRepository(testDbContext);
+            var queryService = new TrademarkSearchQueryService(readRepo);
+
+            var query = new TrademarkSearchQueryDTO
+            {
+                Query = null, // Empty query => no text filter
+                SearchBy = TrademarkSearchBy.Wordmark,
+                Mode = SearchMode.Contains,
+                Page = DefaultPage,
+                PageSize = DefaultPageSize
+            };
+
+            var (queryResult, total) = await queryService.SearchAsync(query, CancellationToken.None);
+            total.Should().Be(3);
+            queryResult.Should().HaveCount(3);
+
+            queryResult.Select(q => q.RegistrationNumber).Should().ContainInOrder("1111111", "2222222", "3333333");
+        }
     }
 }
