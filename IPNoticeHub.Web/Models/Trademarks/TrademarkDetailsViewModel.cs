7460ae45f7d9496c90f2261e27187daad976274e
Nikolay Todorov <everflowing555@gmail.com>
2025-09-24 18:34:42 +0300

    Added TrademarkDetailsViewModel

---
+++ IPNoticeHub.Web/Models/Trademarks/TrademarkDetailsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using IPNoticeHub.Common.EnumConstants;
+
+namespace IPNoticeHub.Web.ViewModels.Trademarks
+{
+    public sealed class TrademarkDetailsViewModel
+    {
+        public int? Id { get; init; }
+        public Guid PublicId { get; init; }
+        public string? Wordmark { get; init; }
+        public string? Owner { get; init; }
+        public string? SourceId { get; init; }
+        public string? RegistrationNumber { get; init; }
+        public TrademarkStatusCategory Status { get; init; }
+        public string? GoodsAndServices { get; init; }
+        public DateTime? FilingDate { get; init; }
+        public DateTime? RegistrationDate { get; init; }
+        public string? MarkImageUrl { get; init; }
+        public DataProvider Provider { get; init; }
+        public IReadOnlyList<int> Classes { get; init; } = Array.Empty<int>();
+        public bool IsInWatchlist { get; init; }
+    }
+}
