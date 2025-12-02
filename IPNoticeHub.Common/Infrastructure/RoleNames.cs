e6fd67961b144364af17bd81a3ddaaef620c87fe
Nikolay Todorov <everflowing555@gmail.com>
2025-12-02 12:22:47 +0200

    Added static class RoleNames with Admin and User

---
+++ IPNoticeHub.Common/Infrastructure/RoleNames.cs
@@ -0,0 +1,8 @@
+﻿namespace IPNoticeHub.Common.Infrastructure
+{
+    public static class RoleNames
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
