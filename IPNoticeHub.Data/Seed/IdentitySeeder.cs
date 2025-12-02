de1c5a61bdf155aa815aab75f4fabe26e7916987
Nikolay Todorov <everflowing555@gmail.com>
2025-12-02 16:17:42 +0200

    Finished IdentitySeeder/SeedIdentitiesAsync

---
+++ IPNoticeHub.Data/Seed/IdentitySeeder.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using IPNoticeHub.Common.Infrastructure;
+
+namespace IPNoticeHub.Data.Seed
+{
+    public class IdentitySeeder
+    {
+        public static async Task SeedIdentitiesAsync(IServiceProvider services)
+        {
+            using var scope = services.CreateScope();
+
+            var roleManager = scope.ServiceProvider.
+                GetRequiredService<RoleManager< IdentityRole >>();
+
+            var userManager = scope.ServiceProvider.
+                GetRequiredService<UserManager< IdentityUser>>();
+
+            var roleNames = new[]
+            {
+                RoleNames.Admin,
+                RoleNames.User
+            };
+
+            foreach (var roleName in roleNames)
+            {
+                bool roleExists = await roleManager.RoleExistsAsync(roleName);
+
+                if (!roleExists)
+                {
+                    var role = new IdentityRole(roleName);
+                    await roleManager.CreateAsync(role);
+                }
+            }
+        }
+    }
+}
