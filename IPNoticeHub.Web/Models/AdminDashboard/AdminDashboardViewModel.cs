6eee4f34fcfa2f771a6b532b89ae95903b28f29d
Nikolay Todorov <everflowing555@gmail.com>
2025-12-03 18:06:29 +0200

    Added AdminDashboardViewModel

---
+++ IPNoticeHub.Web/Models/AdminDashboard/AdminDashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using IPNoticeHub.Data.Entities.ApplicationUser;
+
+namespace IPNoticeHub.Web.Models.AdminDashboard
+{
+    public class AdminDashboardViewModel
+    {
+        public int TotalUsers { get; set; }
+        public int TrademarksAdded { get; set; }
+        public int CopyrightsAdded { get; set; }
+        public int WatchlistedItems { get; set; }
+
+        public ICollection<ApplicationUser> RecentRegistrations { get; set; } = 
+            new List<ApplicationUser>();
+    }
+}
