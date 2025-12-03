8767362728afa66d8a0c64ff61f241a920983cda
Nikolay Todorov <everflowing555@gmail.com>
2025-12-03 21:16:33 +0200

    Added pagination properties to TrademarkSearchResultsViewModel

--- IPNoticeHub.Web/Models/Application/TrademarkSearchResultsViewModel.cs
+++ IPNoticeHub.Web/Models/Application/TrademarkSearchResultsViewModel.cs
@@ -4,6 +4,8 @@ namespace IPNoticeHub.Web.Models.Application
 {
     public class TrademarkSearchResultsViewModel
     {
+        // Search parameters, active filters, and returned results for the trademark search.
+        
         public string? Query { get; set; }
         public TrademarkClass? Class { get; set; }
         public TrademarkStatusCategory? Status { get; set; }
@@ -12,5 +14,13 @@ namespace IPNoticeHub.Web.Models.Application
         public SearchMode? Mode { get; set; }
         public IEnumerable<TmSearchResultSingleItemViewModel> Results { get; set; } = Enumerable.Empty<TmSearchResultSingleItemViewModel>();
         public int Total { get; set; }
+
+
+        // CurrentPage, PageSize and computed TotalPages/HasPreviousPage/HasNextPage providing paging information.
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages => PageSize <= 0 ? 1 : (int)Math.Ceiling(Total / (double)PageSize);
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => CurrentPage < TotalPages;
     }
 }
