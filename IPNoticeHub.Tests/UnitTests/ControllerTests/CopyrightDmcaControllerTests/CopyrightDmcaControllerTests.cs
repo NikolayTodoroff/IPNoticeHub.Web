45beb1ab4a2db61fbff939f2084fafb860b36e0e
Nikolay Todorov <everflowing555@gmail.com>
2025-12-20 21:28:24 +0200

    Added CopyrightDmcaControllerTests

---
+++ IPNoticeHub.Tests/UnitTests/ControllerTests/CopyrightDmcaControllerTests/CopyrightDmcaControllerTests.cs
@@ -0,0 +1,210 @@
+﻿using FluentAssertions;
+using IPNoticeHub.Application.DTOs.CopyrightDTOs;
+using IPNoticeHub.Application.DTOs.PdfDTOs;
+using IPNoticeHub.Application.Templates.Abstractions;
+using IPNoticeHub.Shared.Enums;
+using IPNoticeHub.Web.Models.PdfGeneration;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System.Security.Claims;
+
+namespace IPNoticeHub.Tests.UnitTests.ControllerTests.CopyrightDmcaControllerTests
+{
+    public class CopyrightDmcaControllerTests : BaseCopyrightDmcaControllerTests
+    {
+        [Test]
+        public async Task Dmca_Get_ShouldReturnForbid_WhenUserIdMissing()
+        {
+            controller.ControllerContext.HttpContext!.User =
+                new ClaimsPrincipal(new ClaimsIdentity());
+
+            var result = await controller.Dmca(Guid.NewGuid());
+
+            result.Should().BeOfType<ForbidResult>();
+        }
+
+        [Test]
+        public async Task Dmca_Get_ShouldReturnNotFound_WhenDetailsDtoIsNull()
+        {
+            var publicId = Guid.NewGuid();
+
+            copyrightService.Setup(
+                s => s.GetDetailsAsync(
+                    TestUserId, 
+                    publicId, 
+                    It.IsAny<CancellationToken>())).
+                    ReturnsAsync((CopyrightDetailsDto?)null);
+
+            var result = await controller.Dmca(publicId);
+
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [Test]
+        public async Task Dmca_Get_ShouldReturnView_WithViewModel_AndTemplateBody()
+        {
+            var publicId = Guid.NewGuid();
+
+            var dto = new CopyrightDetailsDto
+            {
+                PublicId = publicId,
+                Title = "Test Work",
+                RegistrationNumber = "REG-123"
+            };
+
+            copyrightService.Setup(
+                s => s.GetDetailsAsync(
+                    TestUserId, 
+                    publicId, 
+                    It.IsAny<CancellationToken>())).
+                    ReturnsAsync(dto);
+
+            letterTemplateProvider.Setup(
+                p => p.GetTemplateByKey("DMCA-General")).
+                Returns(new LetterTemplatePreset(
+                    LetterTemplateType.Dmca,
+                    "DMCA-General",
+                    "DMCA - General",
+                    "DMCA TEMPLATE BODY"));
+
+            var actionResult = await controller.Dmca(publicId);
+
+            actionResult.Should().BeOfType<ViewResult>();
+            var viewResult = (ViewResult)actionResult;
+
+            viewResult.Model.Should().BeOfType<DMCAViewModel>();
+            var viewModel = (DMCAViewModel)viewResult.Model!;
+
+            viewModel.PublicId.Should().Be(publicId);
+            viewModel.BodyTemplate.Should().Be("DMCA TEMPLATE BODY");
+
+            letterTemplateProvider.Verify(
+                p => p.GetTemplateByKey("DMCA-General"), 
+                Times.Once);
+        }
+
+        [Test]
+        public async Task Dmca_Post_ShouldReturnView_WhenModelStateInvalid()
+        {
+            var publicId = Guid.NewGuid();
+            var viewModel = new DMCAViewModel { PublicId = publicId };
+
+            controller.ModelState.AddModelError(
+                "SenderName", 
+                "Required");
+
+            var result = await controller.Dmca(publicId, viewModel);
+
+            result.Should().BeOfType<ViewResult>();
+            var viewResult = (ViewResult)result;
+
+            viewResult.Model.Should().BeSameAs(viewModel);
+
+            copyrightService.Verify(
+                s => s.GetDetailsAsync(
+                    It.IsAny<string>(), 
+                    It.IsAny<Guid>(), 
+                    It.IsAny<CancellationToken>()),
+                Times.Never);
+
+            pdfService.Verify(
+                s => s.GenerateFromInputAsync(
+                    It.IsAny<LetterInputDto>(), 
+                    It.IsAny<CancellationToken>()),
+                Times.Never);
+        }
+
+        [Test]
+        public async Task Dmca_Post_ShouldReturnForbid_WhenUserIdMissing_AndModelStateValid()
+        {
+            controller.ControllerContext.HttpContext!.User =
+                new ClaimsPrincipal(new ClaimsIdentity());
+
+            var publicId = Guid.NewGuid();
+            var viewModel = new DMCAViewModel { PublicId = publicId };
+
+            var result = await controller.Dmca(publicId, viewModel);
+
+            result.Should().BeOfType<ForbidResult>();
+        }
+
+        [Test]
+        public async Task Dmca_Post_ShouldReturnNotFound_WhenDetailsDtoIsNull()
+        {
+            var publicId = Guid.NewGuid();
+            var vm = new DMCAViewModel { PublicId = publicId };
+
+            copyrightService.Setup(
+                s => s.GetDetailsAsync(
+                    TestUserId, 
+                    publicId, 
+                    It.IsAny<CancellationToken>())).
+                    ReturnsAsync((CopyrightDetailsDto?)null);
+
+            var result = await controller.Dmca(publicId, vm);
+
+            result.Should().BeOfType<NotFoundResult>();
+
+            pdfService.Verify(
+                s => s.GenerateFromInputAsync(
+                    It.IsAny<LetterInputDto>(), 
+                    It.IsAny<CancellationToken>()),
+                Times.Never);
+        }
+
+        [Test]
+        public async Task Dmca_Post_ShouldReturnPdfFile_WhenHappyPath()
+        {
+            var publicId = Guid.NewGuid();
+
+            var dto = new CopyrightDetailsDto
+            {
+                PublicId = publicId,
+                Title = "My Copyright Work",
+                RegistrationNumber = "REG-999"
+            };
+
+            copyrightService.Setup(
+                s => s.GetDetailsAsync(
+                    TestUserId, 
+                    publicId, 
+                    It.IsAny<CancellationToken>())).
+                    ReturnsAsync(dto);
+
+            var viewModel = new DMCAViewModel
+            {
+                PublicId = publicId,
+                WorkTitle = "User Title (will be overwritten)",
+                BodyTemplate = "Final body"
+            };
+
+            var pdfBytes = new byte[] { 1, 2, 3, 4 };
+
+            pdfService.Setup(
+                s => s.GenerateFromInputAsync(
+                    It.IsAny<LetterInputDto>(), 
+                    It.IsAny<CancellationToken>())).
+                    ReturnsAsync(pdfBytes);
+
+            var actionResult = await controller.Dmca(
+                publicId, 
+                viewModel);
+
+            actionResult.Should().BeOfType<FileContentResult>();
+            var file = (FileContentResult)actionResult;
+
+            file.ContentType.Should().Be("application/pdf");
+            file.FileContents.Should().BeEquivalentTo(pdfBytes);
+
+            file.FileDownloadName.Should().Contain($"DMCA-{dto.Title}-");
+            file.FileDownloadName.Should().EndWith(".pdf");
+
+            pdfService.Verify(
+                s => s.GenerateFromInputAsync(
+                    It.IsAny<LetterInputDto>(), 
+                    It.IsAny<CancellationToken>()),
+                Times.Once);
+        }
+    }
+}
---
+++ IPNoticeHub.Tests/UnitTests/ControllerTests/CopyrightDmcaControllerTests/RecoverCopyrightDmcaControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPNoticeHub.Tests.UnitTests.ControllerTests.CopyrightDmcaControllerTests
+{
+    public class RecoverCopyrightDmcaControllerTests : BaseCopyrightDmcaControllerTests
+    {
+
+    }
+}
