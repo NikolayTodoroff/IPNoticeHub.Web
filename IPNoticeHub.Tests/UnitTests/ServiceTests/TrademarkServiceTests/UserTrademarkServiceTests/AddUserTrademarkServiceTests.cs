09428bcc80d284c2e0916d4de8b926da62c76c6d
Nikolay Todorov <everflowing555@gmail.com>
2025-12-27 23:58:08 +0200

    Added UserTrademarkServiceBase, fixed AddUserTrademarkServiceTests

--- IPNoticeHub.Tests/UnitTests/ServiceTests/TrademarkServiceTests/UserTrademarkServiceTests/AddUserTrademarkServiceTests.cs
+++ IPNoticeHub.Tests/UnitTests/ServiceTests/TrademarkServiceTests/UserTrademarkServiceTests/AddUserTrademarkServiceTests.cs
@@ -1,119 +1,40 @@
 ﻿using FluentAssertions;
-using IPNoticeHub.Shared.Enums;
-using IPNoticeHub.Application.Repositories.TrademarkRepository;
-using IPNoticeHub.Infrastructure.Persistence.Repositories.TrademarkRepository;
-using IPNoticeHub.Tests.UnitTests.UnitTestFactories;
+using IPNoticeHub.Tests.UnitTests.ServiceTests.TrademarkServiceTests.UserTrademarkServiceTests;
 using NUnit.Framework;
-using IPNoticeHub.Infrastructure.Identity;
-using IPNoticeHub.Application.Services.TrademarkService.Implementations;
 
 namespace IPNoticeHub.Tests.UnitTests.ServiceTests.Trademarks.UserTrademarkServiceTests
 {
-    [TestFixture]
-    public class AddUserTrademarkServiceTests
+    public class AddUserTrademarkServiceTests : UserTrademarkServiceBase
     {
         [Test]
         public async Task AddAsync_WhenNotInCollection_AddsLink_ThenIsInCollectionReturnsTrue()
         {
-            using var testDbContext = 
-                InMemoryDbContextFactory.CreateTestDbContext();
-
-            var user = new ApplicationUser
-            {
-                Id = "user-1",
-                UserName = "newUser",
-                Email = "user1@test.local"
-            };
-
-            var (trademarkEntity, _) = 
-                InMemoryDbContextFactory.CreateTrademark(
-                wordmark: "AAA",
-                owner: "Owner A",
-                goodsAndServices: "testGoodsAndSerices",
-                sourceId: "testSourceId",
-                statusDetail: "testStatusDetail",
-                regNumber: "1234567",
-                status: TrademarkStatusCategory.Registered,
-                source: DataProvider.USPTO,
-                classNumbers: new[] { 9, 35 });
-
-            testDbContext.Users.Add(user);
-
-            testDbContext.TrademarkRegistrations.Add(trademarkEntity);
-            await testDbContext.SaveChangesAsync();
-
-            ITrademarkRepository tmRepository = 
-                new TrademarkRepository(testDbContext);
-
-            IUserTrademarkRepository userTmRepository = 
-                new UserTrademarkRepository(testDbContext);
-
-            var service = new TrademarkCollectionService(
-                tmRepository, 
-                userTmRepository);
-
             await service.AddAsync(
                 userId: user.Id,
                 trademarkId: trademarkEntity.Id,
                 cancellationToken: default);
 
-            bool linkedInCollection = await service.IsInCollectionAsync(
+            bool isInCollection = await service.IsInCollectionAsync(
                 user.Id, 
                 trademarkEntity.Id, 
                 includeSoftDeleted: false, 
                 cancellationToken: default);
 
-            linkedInCollection.Should().BeTrue();
+            isInCollection.Should().BeTrue();
 
-            var existingLink = testDbContext.UserTrademarks.Where
+            var links = 
+                testDbContext.UserTrademarks.Where
                 (x => x.ApplicationUserId == user.Id && 
                 x.TrademarkEntityId == trademarkEntity.Id).
                 ToList();
 
-            existingLink.Should().HaveCount(1);
-            existingLink[0].IsDeleted.Should().BeFalse();
+            links.Should().HaveCount(1);
+            links[0].IsDeleted.Should().BeFalse();
         }
 
         [Test]
         public async Task AddAsync_WhenPreviouslySoftDeleted_UndeletesExistingLink()
         {
-            using var testDbContext = 
-                InMemoryDbContextFactory.CreateTestDbContext();
-
-            var user = new ApplicationUser
-            {
-                Id = "user-1",
-                UserName = "newUser",
-                Email = "user1@test.local"
-            };
-
-            var (trademarkEntity, _) = 
-                InMemoryDbContextFactory.CreateTrademark(
-                wordmark: "AAA",
-                owner: "Owner A",
-                goodsAndServices: "testGoodsAndSerices",
-                sourceId: "testSourceId",
-                statusDetail: "testStatusDetail",
-                regNumber: "1234567",
-                status: TrademarkStatusCategory.Registered,
-                source: DataProvider.USPTO,
-                classNumbers: new[] { 9, 35 });
-
-            testDbContext.Users.Add(user);
-
-            testDbContext.TrademarkRegistrations.Add(trademarkEntity);
-            await testDbContext.SaveChangesAsync();
-
-            ITrademarkRepository tmRepository = 
-                new TrademarkRepository(testDbContext);
-
-            IUserTrademarkRepository userTmRepository = 
-                new UserTrademarkRepository(testDbContext);
-
-            var service = new TrademarkCollectionService(
-                tmRepository, 
-                userTmRepository);
-
             await service.AddAsync(
                 userId: user.Id,
                 trademarkId: trademarkEntity.Id,
@@ -124,122 +45,67 @@ namespace IPNoticeHub.Tests.UnitTests.ServiceTests.Trademarks.UserTrademarkServi
                 trademarkId: trademarkEntity.Id,
                 cancellationToken: default);
 
-            var softDeletedLinks = testDbContext.UserTrademarks.Where(
+            var userTrademark = 
+                testDbContext.UserTrademarks.Where(
                 x => x.ApplicationUserId == user.Id && 
                 x.TrademarkEntityId == trademarkEntity.Id).
                 ToList();
 
-            softDeletedLinks.Should().HaveCount(1);
-            softDeletedLinks[0].IsDeleted.Should().BeTrue();
+            userTrademark.Should().HaveCount(1);
+            userTrademark[0].IsDeleted.Should().BeTrue();
 
             await service.AddAsync(
                userId: user.Id,
                trademarkId: trademarkEntity.Id,
                cancellationToken: default);
 
-            var unDeletedLinks = testDbContext.UserTrademarks.Where(
+            var links = 
+                testDbContext.UserTrademarks.Where(
                 x => x.ApplicationUserId == user.Id && 
                 x.TrademarkEntityId == trademarkEntity.Id).
                 ToList();
 
-            unDeletedLinks.Should().HaveCount(1);
-            unDeletedLinks[index: 0].IsDeleted.Should().BeFalse();
+            links.Should().HaveCount(1);
+            links[index: 0].IsDeleted.Should().BeFalse();
 
-            var linkedInCollection = await service.IsInCollectionAsync(
+            var isInCollection = await service.IsInCollectionAsync(
                 user.Id, 
                 trademarkEntity.Id, 
                 includeSoftDeleted: false, 
                 cancellationToken: default);
 
-            linkedInCollection.Should().BeTrue();
+            isInCollection.Should().BeTrue();
         }
 
         [Test]
         public async Task AddAsync_WhenTrademarkIdDoesNotExist_DoesNothing()
         {
-            using var testDbContext =
-                InMemoryDbContextFactory.CreateTestDbContext();
-
-            var user = new ApplicationUser
-            {
-                Id = "user-1",
-                UserName = "newUser",
-                Email = "user1@test.local"
-            };
-
-            testDbContext.Users.Add(user);
-
-            await testDbContext.SaveChangesAsync();
-
-            ITrademarkRepository tmRepository =
-                new TrademarkRepository(testDbContext);
-
-            IUserTrademarkRepository userTmRepository =
-                new UserTrademarkRepository(testDbContext);
-
-            var service = new TrademarkCollectionService(
-                tmRepository,
-                userTmRepository);
-
             await service.AddAsync(
                 user.Id,
                 1234567,
                 default);
 
             testDbContext.UserTrademarks.Where(
-                x => x.ApplicationUserId == user.Id).Should().BeEmpty();
+                x => x.ApplicationUserId == user.Id).
+                Should().BeEmpty();
         }
 
         [Test]
         public async Task AddAsync_WhenAlreadyLinkedInCollection_DoesNotCreateDuplicateRow()
         {
-            using var testDbContext =
-                InMemoryDbContextFactory.CreateTestDbContext();
-
-            var user = new ApplicationUser
-            {
-                Id = "user-1",
-                UserName = "newUser",
-                Email = "user1@test.local"
-            };
-
-            var (tmEntity, _) = InMemoryDbContextFactory.CreateTrademark(
-                wordmark: "AAA",
-                owner: "Owner A",
-                goodsAndServices: "testGoodsAndSerices",
-                sourceId: "testSourceId",
-                statusDetail: "testStatusDetail",
-                regNumber: "1234567",
-                status: TrademarkStatusCategory.Registered,
-                source: DataProvider.USPTO,
-                classNumbers: new[] { 25 });
-
-            testDbContext.Users.Add(user);
-            testDbContext.TrademarkRegistrations.Add(tmEntity);
-
-            await testDbContext.SaveChangesAsync();
-
-            ITrademarkRepository tmRepository = new
-                TrademarkRepository(testDbContext);
-
-            IUserTrademarkRepository userTmRepository =
-                new UserTrademarkRepository(testDbContext);
-
-            var service = new TrademarkCollectionService(tmRepository, userTmRepository);
-
             await service.AddAsync(
                 user.Id,
-                tmEntity.Id,
+                trademarkEntity.Id,
                 default);
 
             await service.AddAsync(
                 user.Id,
-                tmEntity.Id,
+                trademarkEntity.Id,
                 default);
 
             var links = testDbContext.UserTrademarks.
                 Where(x => x.ApplicationUserId == user.Id &&
-                x.TrademarkEntityId == tmEntity.Id).
+                x.TrademarkEntityId == trademarkEntity.Id).
                 ToList();
 
             links.Should().HaveCount(1);
---
+++ IPNoticeHub.Tests/UnitTests/ServiceTests/TrademarkServiceTests/UserTrademarkServiceTests/UserTrademarkServiceBase.cs
@@ -0,0 +1,74 @@
+﻿using IPNoticeHub.Application.Repositories.TrademarkRepository;
+using IPNoticeHub.Application.Services.TrademarkService.Implementations;
+using IPNoticeHub.Domain.Entities.Trademarks;
+using IPNoticeHub.Infrastructure.Identity;
+using IPNoticeHub.Infrastructure.Persistence;
+using IPNoticeHub.Infrastructure.Persistence.Repositories.TrademarkRepository;
+using IPNoticeHub.Shared.Enums;
+using IPNoticeHub.Tests.UnitTests.UnitTestFactories;
+using NUnit.Framework;
+
+namespace IPNoticeHub.Tests.UnitTests.ServiceTests.TrademarkServiceTests.UserTrademarkServiceTests
+{
+    public class UserTrademarkServiceBase
+    {
+        protected IPNoticeHubDbContext testDbContext = null!;
+        protected ApplicationUser user = null!;
+        protected ITrademarkRepository tmRepository = null!;
+        protected IUserTrademarkRepository userTmRepository = null!;
+        protected TrademarkCollectionService service = null!;
+
+        protected TrademarkEntity trademarkEntity = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testDbContext =
+                InMemoryDbContextFactory.CreateTestDbContext();
+
+            user = InMemoryDbContextFactory.CreateApplicationUser();
+            testDbContext.Users.Add(user);
+
+            var testTrademarks =
+                TestTrademarkData.CreateTestTrademarks(
+                    out trademarkEntity);
+
+            testDbContext.TrademarkRegistrations.AddRange(testTrademarks);
+            testDbContext.SaveChangesAsync();
+
+            tmRepository = new TrademarkRepository(testDbContext);
+            userTmRepository = new UserTrademarkRepository(testDbContext);
+
+            service = new TrademarkCollectionService(
+                tmRepository,
+                userTmRepository);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            testDbContext?.Dispose();
+        }
+
+        protected static class TestTrademarkData
+        {
+            public static TrademarkEntity CreateTestTrademarks(
+                out TrademarkEntity trademarkEntity)
+            {
+                (trademarkEntity, _) =
+                InMemoryDbContextFactory.CreateTrademark(
+                wordmark: "AAA",
+                owner: "Owner A",
+                goodsAndServices: "testGoodsAndSerices",
+                sourceId: "testSourceId",
+                statusDetail: "testStatusDetail",
+                regNumber: "1234567",
+                status: TrademarkStatusCategory.Registered,
+                source: DataProvider.USPTO,
+                classNumbers: new[] { 9, 35 });
+
+                return trademarkEntity;
+            }
+        }
+    }
+}
