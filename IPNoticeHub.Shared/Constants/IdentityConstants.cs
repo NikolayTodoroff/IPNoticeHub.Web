ca16e5429d6917c56009dd84f63469e553f41d65
Nikolay Todorov <everflowing555@gmail.com>
2026-01-14 09:33:02 +0200

    Added DemoAccountCredentials to IdentityConstants

--- IPNoticeHub.Shared/Constants/IdentityConstants.cs
+++ IPNoticeHub.Shared/Constants/IdentityConstants.cs
@@ -7,6 +7,11 @@
             public const string AdminEmailAddress = "admin@ipnoticehub.com";
             public const string AdminEmailPassword = "Admin!234";
         }
+        public static class DemoAccountCredentials
+        {
+            public const string DemoEmailAddress = "demo@ipnoticehub.com";
+            public const string DemoEmailPassword = "Demo!234";
+        }
 
         public static class AuthRedirectPaths
         {
